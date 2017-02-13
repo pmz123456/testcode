bf322c86e55bc4fb5da674c0b3ba713975d825d1
penmingzhu <penmingzhu@tongtongmall.com>
2017-02-13 10:55:38 +0800

    初始化

---
+++ TestDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a test");
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
